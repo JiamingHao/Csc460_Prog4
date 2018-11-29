992f255c15a7a33a4d8ecd85a96d5a516179efbb
Mingcheng Dai <dmcccccc@gmail.com>
2018-11-29 10:43:55 -0700

    upload ER-Diagram and WebController

--- src/main/java/hello/WebController.java
+++ src/main/java/hello/WebController.java
@@ -27,6 +27,8 @@ public class WebController {
   @Autowired
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
+    // TODO update table prefix below.
+    private String prefix = "";
 
     @PostConstruct
     private void postConstruct() {
@@ -41,11 +43,36 @@ public class WebController {
 
     @PostMapping("/addPatient")
     public String patientSubmit(@ModelAttribute Patient patient) {
-	// TODO add db query here.
+        jdbcTemplate.update("insert into ?.patient values (?, ?, ?, ?, ?, ?, ?)", prefix, patient.getPid(), patient.getLastName(), patient.getFirstName(), patient.getGender(), patient.getDate_of_birth(), patient.getAddress(), patient.getContactNumber());
         return "resultPatient";
     }
 
-	
+    @GetMapping("/deletePatient")
+    public String patientForm(Model model) {
+        model.addAttribute("patient", new Patient());
+        return "deletePatient";
+    }
+
+    @PostMapping("/deletePatient")
+    public String patientSubmit(@ModelAttribute Patient patient) {
+        // TODO add db query here.
+        return "deletePatientResult";
+    }
+
+    @GetMapping("/updatePatient")
+    public String patientForm(Model model) {
+        model.addAttribute("patient", new Patient());
+        return "updatePatient";
+    }
+
+    @PostMapping("/updatePatient")
+    public String patientSubmit(@ModelAttribute Patient patient) {
+        // TODO add db query here.
+        return "updatePatientResult";
+    }
+
+
+
     @GetMapping("/addDoctor")
     public String doctorForm(Model model){
 	model.addAttribute("doctor", new Doctor());
@@ -57,7 +84,33 @@ public class WebController {
 	// TODO add db query here.
  	return "resultDoctor";
     }
-    
+
+    @GetMapping("/deleteDoctor")
+    public String DoctorForm(Model model) {
+        model.addAttribute("Doctor", new Doctor());
+        return "deleteDoctor";
+    }
+
+    @PostMapping("/deleteDoctor")
+    public String DoctorSubmit(@ModelAttribute Doctor doctor) {
+        // TODO add db query here.
+        return "deleteDoctorResult";
+    }
+
+    @GetMapping("/updateDoctor")
+    public String DoctorForm(Model model) {
+        model.addAttribute("Doctor", new Doctor());
+        return "updateDoctor";
+    }
+
+    @PostMapping("/updateDoctor")
+    public String DoctorSubmit(@ModelAttribute Doctor doctor) {
+        // TODO add db query here.
+        return "updateDoctorResult";
+    }
+
+
+
     @GetMapping("/addNurse")
     public String nurseForm(Model model){
 	model.addAttribute("nurse", new Nurse());
@@ -70,6 +123,32 @@ public class WebController {
  	return "resultNurse";
     }
 
+    @GetMapping("/deleteNurse")
+    public String NurseForm(Model model) {
+        model.addAttribute("Nurse", new Nurse());
+        return "deleteNurse";
+    }
+
+    @PostMapping("/deleteNurse")
+    public String NurseSubmit(@ModelAttribute Nurse nurse) {
+        // TODO add db query here.
+        return "deleteNurseResult";
+    }
+
+    @GetMapping("/updateNurse")
+    public String NurseForm(Model model) {
+        model.addAttribute("Nurse", new Nurse());
+        return "updateNurse";
+    }
+
+    @PostMapping("/updateNurse")
+    public String NurseSubmit(@ModelAttribute Nurse nurse) {
+        // TODO add db query here.
+        return "updateNurseResult";
+    }
+
+
+
     @GetMapping("/addStaff")
     public String staffForm(Model model){
 	model.addAttribute("staff", new Staff());
@@ -81,4 +160,46 @@ public class WebController {
 	// TODO add db query here.
  	return "resultStaff";
     }
+
+    @GetMapping("/deleteStaff")
+    public String StaffForm(Model model) {
+        model.addAttribute("Staff", new Staff());
+        return "deleteStaff";
+    }
+
+    @PostMapping("/deleteStaff")
+    public String StaffSubmit(@ModelAttribute Staff staff) {
+        // TODO add db query here.
+        return "deleteStaffResult";
+    }
+
+    @GetMapping("/updateStaff")
+    public String StaffForm(Model model) {
+        model.addAttribute("Staff", new Staff());
+        return "updateStaff";
+    }
+
+    @PostMapping("/updateStaff")
+    public String StaffSubmit(@ModelAttribute Staff staff) {
+        // TODO add db query here.
+        return "updateStaffResult";
+    }
+
+    /* TODO add&update patient treatment record.
+    When adding PID, appointment number, reason of visit, and date of visit cannot be left as blank.
+    When updating, everything can be updated except the PID, appointment number, reason of visit, and the date of visit.
+    */
+
+
+
+    /* TODO add&update cashier record.
+    When adding, only the payment date can be left as blank.
+    The update must not change the PID where as other records should be allowed to modify including the EID.
+    */
+
+
+
+    /* TODO Update department room number and building name from the department table.
+    In fact, anything in this table can be modified and updated except the DID.
+    */
 }
