685de216ae2b549600ae2bfc63d43d051a32252b
SiyiJiang <siyijiang@email.arizona.edu>
2018-12-03 00:02:50 -0700

    upload query4result

---
+++ src/main/java/hello/Query4result.java
@@ -0,0 +1,33 @@
+package hello;
+
+public class Query4result {
+	private int pid;
+	private String lastName;
+	private String firstName;
+	private String medicineName;
+	public int getPid() {
+		return pid;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMedicineName() {
+		return medicineName;
+	}
+	public void setMedicineName(String medicineName) {
+		this.medicineName = medicineName;
+	}
+	
+}
