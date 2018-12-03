3e2b697fe2403917c257180942b5b1278a7ab3d5
JiamingHao <hao1@email.arizona.edu>
2018-12-02 20:22:02 -0700

    Modify the display format of Query1 Result.

--- src/main/java/hello/Query1result.java
+++ src/main/java/hello/Query1result.java
@@ -10,6 +10,7 @@ public class Query1result {
 	private String visitReason;
 	private String treatmentMethod;
 	private int did;
+	private String fullName;
 	
 	// Used to record exception info if necessary
 	private String errorMsg;
@@ -75,6 +76,11 @@ public class Query1result {
 	public void setDid(int did) {
 		this.did = did;
 	}
+
+	public String getFullName()
+	{
+		return this.firstName + " " + this.lastName;
+	}	
 	
 	
 }
